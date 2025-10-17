d54cbadf327a6aec70a87e2ce4596b4c7079e525
Robert <168944227+bobbiehiv@users.noreply.github.com>
2025-10-17 17:46:02 -0400

    Update sho_run.py

--- sho_run.py
+++ sho_run.py
@@ -2,56 +2,42 @@
 
 from netmiko import ConnectHandler
 
-import subprocess
-
 import getpass
 
-passwd = getpass.getpass('Please enter the password: ')   # getpass the password and secret
+username = input ('Username: ')
+passwd = getpass.getpass('password: ')          # getpass the password and secret
+deviceip = input ('ip or domain name?: ')
+# enable = getpass.getpass('Please enter the enable secret: ')
 
-enable = getpass.getpass('Please enter the enable secret: ')
-
-def Username():                     # use this function to narrow 
-    Posistion_counter = 0           # down whoami windows command
-    Result = subprocess.getoutput("whoami")
-    for words in Result: 
-        if words == "\\":
-            Posistion = Posistion_counter
-            break
-        Posistion_counter += 1
-    return Result[Posistion + 1: ]
-Local_Computer_Username = Username()
-
-# print(Local_Computer_Username)
-
-Net_device = {                         # Creates connection to router
-    "device_type": "cisco_ios",        # Device type
-    "ip": "192.168.1.230",             # Router IP
-    "username": "nixon",               
-    "password": passwd,                # Getpass password 
-    "secret": enable                   # Getpass secret
+Net_device = {                                  # creates connection to router
+    "device_type": "cisco_ios",                 # device type
+    "ip": deviceip,                             # device IP or (use varible)
+    "username": username,               
+    "password": passwd,                         # Getpass password 
+    "secret": passwd                            # Getpass secret
 }
+
+device = input("what is the device?: ")
 
 Connect_Device = ConnectHandler (**Net_device)
 
 Connect_Device.enable()
 
-Issue =  'show run'                    # Issue a command here 
+Issue =  'show run'                                                 # Issue commands here 
+Issue2 = 'show ip int br'
 
 Output = Connect_Device.send_command(Issue)
 
-with open('C:\\Cisco\\Output texts\\shorun.txt', "w") as f:  # where the text will be created
-    f.write('Router1 ' +  Issue)
-    f.write('\n')
-    f.write(Connect_Device.send_command(Issue))
-    f.write('\n')
+with open(f'C:\\output\\{device}_{deviceip}.txt', "w") as f:        # where the text will be created
+    f.write(f'\n{device}_{deviceip}\n')
+    f.write(f'\n{Issue}\n')                                         # prints command 
+    f.write('-'*75 + '\n')
+    f.write(Connect_Device.send_command(Issue) +'\n')               # issues command
+    f.write(f'{Issue2}\n')                                          # issues second command
+    f.write('-'*75 + '\n')
+    f.write(Connect_Device.send_command(Issue2) +'\n')
     f.write('\nEnd of File')
-
-print(Output)
-print('\n')
+#print('\n')
 print('Completed')
 
-
 Connect_Device.disconnect
-
-
-
